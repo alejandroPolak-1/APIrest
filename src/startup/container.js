f058da5db733fea9c0c9501b57a6e9921db482b5
alejandroPolak <alejandro_polak@hotmail.com>
2020-04-02 11:01:15 -0300

    [container] import models & inyecto en el register

--- src/startup/container.js
+++ src/startup/container.js
@@ -1,33 +1,43 @@
-const {createContainer, asClass, asValue, asFunction} = require('awilix');
+const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
 //config
-const config = require("../config")
-const appServer = require('.')
+const config = require("../config");
+const appServer = require(".");
 
 //services
-const { HomeService } = require('../services');
+const { HomeService } = require("../services");
 
 //controller
-const { HomeController } = require('../controllers')
+const { HomeController } = require("../controllers");
 
 //routes
-const { HomeRoutes  } = require('../routes/index.routes')
-const Routes = require('../routes')
+const { HomeRoutes } = require("../routes/index.routes");
+const Routes = require("../routes");
+
+//models
+const { Comment, Idea, User } = require("../models");
+
 const container = createContainer();
 
 container
-.register({
-    appServer: asClass(appServer).singleton(),
-   router: asFunction(Routes).singleton(),
-   config: asValue(config)
-})
-.register({
-    HomeService: asClass(HomeService).singleton()
-}).register({
-    HomeController: asClass(HomeController.bind(HomeController)).singleton()
-}).register({
-    HomeRoutes: asFunction(HomeRoutes).singleton()
-})
+	.register({
+		appServer: asClass(appServer).singleton(),
+		router: asFunction(Routes).singleton(),
+		config: asValue(config)
+	})
+	.register({
+		HomeService: asClass(HomeService).singleton()
+	})
+	.register({
+		HomeController: asClass(HomeController.bind(HomeController)).singleton()
+	})
+	.register({
+		HomeRoutes: asFunction(HomeRoutes).singleton()
+	})
+	.register({
+		User: asValue(User),
+		Idea: asValue(Idea),
+		Comment: asValue(Comment)
+	});
 
-
-module.exports = container; 
+module.exports = container;
