8b540d7206599176a2e253cc34fee77945b96865
alejandroPolak <alejandro_polak@hotmail.com>
2020-04-08 18:00:54 -0300

    [Correccion names]

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,14 +7,14 @@ class AuthController {
 
 	async signUp(req, res) {
 		const { body } = req;
-		const createUser = await _authService.signUp(body);
+		const createdUser = await _authService.signUp(body);
 		return res.status(201).send(createdUser);
 	}
 
 	async signIn(req, res) {
 		const { body } = req;
 		const creds = await _authService.signIn(body);
-		return send.status(200).send(creds);
+		return res.status(200).send(creds);
 	}
 }
 
