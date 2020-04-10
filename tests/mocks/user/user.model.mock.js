517eb6cbb8b2061a293fa872e7abf952b0787618
alejandroPolak <alejandro_polak@hotmail.com>
2020-04-10 19:05:14 -0300

    [Prueba Unitaria Unit Test] 1- jest.conf.js -> info test de node 2-[tests] para Usuarios            I- [mooks]  datos para prueba             a- [MockUser] de Model (del user.model) usuario y usuarios                             y Repository (probamos sus funciones)           b- index.js           II- [Unit] entidades a testear                [repositories]                      user.repositoty.test -> swit de test 3- [package.json] -> modificar el "script": "test":"jest"

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+      _id: "507f191e810c19729de860ea",
+      name: "ale",
+      username: "ale",
+      password: "superPaster"
+    },
+    users: [
+      {
+        _id: "507f191e810c19729de860ea",
+        name: "ale",
+        username: "ale",
+        password: "superPaster"
+      },
+      {
+        _id: "507f191e810c19729de860eb",
+        name: "Milena",
+        username: "Milena",
+        password: "superPaster"
+      }
+    ]
+  };
